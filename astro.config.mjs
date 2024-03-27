281c4ce7b087bf3e9b8ff78562a937551dfe5f76
anvimaa <70594573+anvimaa@users.noreply.github.com>
2024-03-27 20:29:12 +0100

    update astro config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -23,7 +23,7 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
+  site: "https://minimal-blog-six.vercel.app/",
   base: "/",
   integrations: [
     tailwind(),
